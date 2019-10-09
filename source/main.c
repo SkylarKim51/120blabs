8fda0285dbd825259ffb3ad8c860e67a9009221c
Skylar Kim <skim370@wch136-02.cs.ucr.edu>
2019-10-09 06:13:08 -0700

    First commit

---
+++ source/main.c
@@ -0,0 +1,25 @@
+/*	Author: skim370
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA = 0x00;	PORTA = 0xFF;
+	DDRB = 0x00;	PORTB = 0xFF;
+	
+    /* Insert your solution below */
+    while (1) {
+	
+    }
+    return 1;
+}
