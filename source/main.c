3c3e5f168fe2e09ad091af39ac8c6f14d2fb630c
Skylar Kim <skim370@wch136-02.cs.ucr.edu>
2019-10-09 18:03:34 -0700

    Completed part 2

--- source/main.c
+++ source/main.c
@@ -1,7 +1,7 @@
 /*	Author: skim370
  *  Partner(s) Name: 
  *	Lab Section:023
- *	Assignment: Lab #3  Exercise #1
+ *	Assignment: Lab #3  Exercise #2
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
